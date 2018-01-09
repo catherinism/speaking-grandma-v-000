24f1f75bc9f80cf86f7d33ff992c4c27ba2f7cf0
Catherine Salazar <catcreates13@gmail.com>
2018-01-09 01:12:56 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,9 +5,8 @@ def speak_to_grandma(phrase)
     puts "NO, NOT SINCE 1938!"
   elsif
     speak_to_grandma(phrase) == "I LOVE YOU GRANDMA!"
-
+    puts "I LOVE YOU TOO PUMPKIN!"
   end
-  puts "I LOVE YOU TOO PUMPKIN!"
 end
 
 
