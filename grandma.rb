ca05727e649f5f1a3fcef9a15da602dd01ab4aa1
Catherine Salazar <catcreates13@gmail.com>
2018-01-09 01:33:01 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.lowercase || phrase.capitalize
+  elsif phrase == phrase.lowercase
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
