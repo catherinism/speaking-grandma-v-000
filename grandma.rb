60080672fba1867b668490f784231b64ab6b1cfc
Catherine Salazar <catcreates13@gmail.com>
2018-01-09 01:41:23 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
-  else phrase == phrase.downcase
+  else phrase.downcase
     "HUH?! SPEAK UP, SONNY!"
   end
 end
