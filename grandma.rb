617addeae908e11aee36e7a25b65b1eda00b04ed
Catherine Salazar <catcreates13@gmail.com>
2018-01-09 01:15:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(phrase)
-  if speak_to_grandma(phrase)
+  if speak_to_grandma(phrase) == (phrase).downcase
     puts "HUH?! SPEAK UP, SONNY!"
   elsif speak_to_grandma(phrase).uppercase
     puts "NO, NOT SINCE 1938!"
